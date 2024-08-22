0fbe106e64e8934d1bc7a25ca1d091217ec7b6c1
Daniel Abati <danielabatibabatunde1@gmail.com>
2024-08-22 10:24:26 +0100

    revamped-ui

--- client/frontend/src/components/Navbar.jsx
+++ client/frontend/src/components/Navbar.jsx
@@ -4,9 +4,9 @@ import { Link as RouterLink } from "react-router-dom";
 
 export const Navbar = () => {
   return (
-    <div className="w-full justify-between items-center flex py-4 px-6 sm:py-6 sm:px-8 lg:py-8 lg:px-12">
-      <p className="font-Playfair font-bold text-[20px] sm:text-[24px] lg:text-[32px]">
-        Mayflower
+    <div className="sticky top-0 z-10 w-full justify-between items-center flex py-6 px-6  sm:px-8lg:px-12">
+      <p className="font-Playfair font-bold text-[20px] sm:text-[24px] lg:text-[32px] cursor-pointer">
+        <RouterLink to="/">Mayflower</RouterLink>
       </p>
       <div className="font-Playfair">
         <ul className="flex flex-col sm:flex-row items-center gap-3 sm:gap-5 tracking-wide text-[#c8c8c8]">
@@ -25,18 +25,19 @@ export const Navbar = () => {
             </Link>
           </li>
           <li className="relative pb-1 sm:pb-2">
-            <Link to="contact" smooth={true} duration={500} offset={-70}>
-              <span className="cursor-pointer text-[16px] sm:text-[18px] font-bold pb-2 hover:border-b border-transparent border-b-white transition-all duration-100">
-                Contact Us
-              </span>
-            </Link>
-          </li>
-          <li className="relative pb-1 sm:pb-2">
             <RouterLink to="/booking">
               <span className="cursor-pointer text-[16px] sm:text-[18px] font-bold pb-2 hover:border-b border-transparent border-b-white transition-all duration-100">
                 Events
               </span>
             </RouterLink>
+          </li>
+
+          <li className="relative">
+            <Link to="tour" smooth={true} duration={500} offset={-70}>
+              <button className="bg-transparent border-[2px] border-[#CC5500] py-[6px] px-[10px] sm:py-[8px] sm:px-[10px] lg:py-[10px] lg:px-[12px] rounded-lg text-[16px] sm:text-[12px] lg:text-[16px] font-semibold font-Inter w-[70px] sm:w-[70px] lg:w-[120px]">
+                <RouterLink to="/booking">Book a tour</RouterLink>
+              </button>
+            </Link>
           </li>
         </ul>
       </div>
--- client/frontend/src/page/AdminLogin.jsx
+++ client/frontend/src/page/AdminLogin.jsx
@@ -1,7 +1,4 @@
-import React, { useState } from "react";
-import { Eyeoff } from "../icons/Eyeoff";
-import { Eyeon } from "../icons/Eyeon";
-import { adminMakeRequest } from "../helper/makeRequest";
+import React from "react";
 import admin from "../assets/Images/admin.png";
 
 const AdminLogin = () => {
@@ -12,17 +9,12 @@ const AdminLogin = () => {
       console.error("Error: ", error);
     }
   };
-
-  const [showPassword, setShowPassword] = useState(false);
-
-  const togglePasswordVisibility = () => {
-    setShowPassword(!showPassword);
-  };
-
   return (
     <main className="min-h-screen w-full px-8 py-7 bg-[#212121] text-white flex flex-col items-start justify-between gap-14">
-      <div>
-        <p className=" text-[32px] font-Playfair font-bold">Mayflower</p>
+      <div className="w-full">
+        <p className=" text-[32px] font-Playfair text-center font-bold">
+          Mayflower
+        </p>
       </div>
       <div className=" w-full flex justify-between gap-8 max-h-screen items-center">
         <div className="w-[60%] flex flex-col items-start justify-start">
@@ -36,69 +28,16 @@ const AdminLogin = () => {
           <h2 className="text-[32px] font-bold font-Montserrat text-center text-white mb-8">
             Admin Login
           </h2>
-          <form>
-            <div className="mb-4">
-              <label
-                className="block text-white text-lg font-light font-Montserrat mb-2"
-                htmlFor="email"
-              >
-                Email
-              </label>
-              <input
-                className="w-full px-3 py-2 text-gray-900 rounded border-none focus:outline-none"
-                id="email"
-                type="email"
-                placeholder="Value"
-              />
-            </div>
-            <div className="mb-4">
-              <label
-                className="block text-white text-lg font-light font-Montserrat mb-2"
-                htmlFor="password"
-              >
-                Password
-              </label>
-              <div className="relative">
-                <input
-                  className="w-full px-3 py-2 text-gray-900 rounded border-none focus:outline-none"
-                  id="password"
-                  type={showPassword ? "text" : "password"}
-                  placeholder="Value"
-                />
-                <span
-                  className="absolute inset-y-0 right-0 pr-3 flex items-center text-gray-700 cursor-pointer"
-                  onClick={togglePasswordVisibility}
-                >
-                  {showPassword ? <Eyeon /> : <Eyeoff />}
-                </span>
-              </div>
-            </div>
-
-            <div className="mb-4 text-left">
-              <a
-                href="/"
-                className="text-white text-lg font-Montserrat font-semibold"
-              >
-                Forgot password?
-              </a>
-            </div>
-            <button className="w-full bg-orange-500 text-white text-lg font-Montserrat font-semibold py-2 px-4 rounded hover:bg-orange-600">
-              Login
-            </button>
-            <div className="flex items-center justify-center my-4">
-              <span className="border-t border-[#AEAEAE] w-full"></span>
-              <span className="px-3 font-Montserrat font-bold text-[#AEAEAE]">
-                or
-              </span>
-              <span className="border-t border-[#AEAEAE] w-full"></span>
-            </div>
-            <button
-              className="w-full bg-transparent text-[#CC5500] text-lg font-Montserrat font-semibold py-2 px-4 rounded border border-[#CC5500] hover:bg-[#CC5500] hover:text-white hover:border-black"
-              onClick={handleClick}
-            >
-              Create an account with Google
-            </button>
-          </form>
+          <div className="flex items-center justify-center my-4">
+            <span className="border-t border-[#AEAEAE] w-full"></span>
+            <span className="border-t border-[#AEAEAE] w-full"></span>
+          </div>
+          <button
+            className="w-full bg-transparent text-[#CC5500] text-lg font-Montserrat font-semibold py-2 px-4 rounded border border-[#CC5500] hover:bg-[#CC5500] hover:text-white hover:border-black"
+            onClick={handleClick}
+          >
+            Login with Google
+          </button>
         </div>
       </div>
     </main>
