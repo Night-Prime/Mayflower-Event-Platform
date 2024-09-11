bae10036d720fadb44a109b01fe5493bcd27bd13
Daniel Abati <danielabatibabatunde1@gmail.com>
2024-09-11 21:04:32 +0100

    handle protected route

--- api/src/routes/booking.route.js
+++ api/src/routes/booking.route.js
@@ -10,10 +10,20 @@ const isAuth = require("../middleware/authenticator");
 const router = express.Router();
 
 router.get("/all", isAuth, bookingController.getAllBookings);
-router.post("/", createBookingValidation, bookingController.createBooking);
-router.get("/id", bookingController.getBookingById);
+router.post(
+  "/",
+  isAuth,
+  createBookingValidation,
+  bookingController.createBooking
+);
+router.get("/:id", isAuth, bookingController.getBookingById);
 router.put("/event", bookingController.createEvents);
-router.patch("/", updateBookingValidation, bookingController.updateBooking);
-router.delete("/:id", bookingController.deleteBooking);
+router.patch(
+  "/",
+  isAuth,
+  updateBookingValidation,
+  bookingController.updateBooking
+);
+router.delete("/:id", isAuth, bookingController.deleteBooking);
 
 module.exports = router;
